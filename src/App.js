ebb4dd8e3c3222facf20d5afdba2b793456b0b84
Liz P <parisielizabeth@gmail.com>
2020-12-10 22:12:40 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends Component {
 		// your code in the return statement below!
 		return (
 			<div className="App">
-			  
 				<CatComponent />
 				<GraceHopperQuoteComponent />
-				{/* one more component missing */}<MouseComponent />
+				{/* one more component missing */}
+				<MouseComponent />
 			</div>
 		);
 	}
